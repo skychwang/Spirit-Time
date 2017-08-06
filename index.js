64d310f49045c7cc73a4cdda2981969b7de598f0
skywang329 <sky.wang@yahoo.com>
2017-08-07 02:59:08 +0800

    layout with bootstrap

---
+++ index.js
@@ -0,0 +1,26 @@
+var http = require('http'),
+  request = require('request'),
+	express = require('express'),
+	path = require('path'),
+  querystring = require("querystring");
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.bodyParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/favicon.ico', function(req, res) {
+    res.send(204);
+});
+
+app.get('/', function(req, res) {
+  res.render('main');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
